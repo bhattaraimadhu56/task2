1ac7e8576e246cb9c3fddd8b92099e995deeeb4c
Madhu <bhattaraimadhu56@gmail.com>
2019-08-23 10:11:16 +1200

    Adding New job by Employer

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
@@ -1,19 +1,60 @@
 ﻿import React from 'react';
 import Cookies from 'js-cookie';
-import { Popup } from 'semantic-ui-react';
-import moment from 'moment';
-
 export class JobSummaryCard extends React.Component {
     constructor(props) {
         super(props);
+
+        /*Properties*/
+        const details = props.details ?
+            Object.assign({}, props.details)
+            : {
+                jobData: {
+                    title: " ",
+                    description: " ",
+                    location: { country: " ", city: " " }
+                    
+                }
+            }
+
+        this.state = {
+                        newJob: details
+        }
+        
+
         this.selectJob = this.selectJob.bind(this)
     }
 
     selectJob(id) {
         var cookies = Cookies.get('talentAuthToken');
-        //url: 'http://localhost:51689/listing/listing/closeJob',
+        
     }
 
     render() {
+        return (
+            <div className="ui cards">
+                <div className="card">
+                    <div className="content">
+                        <div className="header"> {this.state.newJob.title} </div>
+                        <div className="ui black right ribbon label"><i className="user icon"></i></div>
+                        <div className="meta"> {this.state.newJob.location} </div>
+                        <div className="description"> <p>{this.state.newJob.description}</p></div>
+                        <div className="extra content">
+                            <div className="right floated">
+                                <div className="ui primary basic buttons">
+                                    <button className="ui button"><i className="window close outline icon"></i>Close</button>
+                                    <button className="ui button"><i className="edit icon"></i>Edit</button>
+                                    <button className="ui button"><i className="copy icon"></i>Copy</button>
+                                </div>
+                            </div>
+                            <div className="left floated">
+                                <button className="negative ui left button">Expired</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            );
+        
     }
 }
+export default JobSummaryCard;
--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
@@ -5,7 +5,10 @@ import LoggedInBanner from '../../Layout/Banner/LoggedInBanner.jsx';
 import { LoggedInNavigation } from '../../Layout/LoggedInNavigation.jsx';
 import { JobSummaryCard } from './JobSummaryCard.jsx';
 import { BodyWrapper, loaderData } from '../../Layout/BodyWrapper.jsx';
-import { Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment } from 'semantic-ui-react';
+import moment from 'moment';
+import { Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment, CardGroup, Card, Label, Button } from 'semantic-ui-react';
+
+//
 
 export default class ManageJob extends React.Component {
     constructor(props) {
@@ -13,9 +16,12 @@ export default class ManageJob extends React.Component {
         let loader = loaderData
         loader.allowedUsers.push("Employer");
         loader.allowedUsers.push("Recruiter");
-        //console.log(loader)
         this.state = {
-            loadJobs: [],
+            loadJobs: [{
+                title: '',
+                summary: '',
+                location: { country: '', city: '' }
+            }],
             loaderData: loader,
             activePage: 1,
             sortBy: {
@@ -24,7 +30,6 @@ export default class ManageJob extends React.Component {
             filter: {
                 showActive: true,
                 showClosed: false,
-                showDraft: true,
                 showExpired: true,
                 showUnexpired: true
             },
@@ -34,30 +39,62 @@ export default class ManageJob extends React.Component {
         this.loadData = this.loadData.bind(this);
         this.init = this.init.bind(this);
         this.loadNewData = this.loadNewData.bind(this);
-        //your functions go here
+               
     };
 
     init() {
-        let loaderData = TalentUtil.deepCopy(this.state.loaderData)
-        loaderData.isLoading = false;
-        this.setState({ loaderData });//comment this
-
-        //set loaderData.isLoading to false after getting data
-        //this.loadData(() =>
-        //    this.setState({ loaderData })
-        //)
-        
-        //console.log(this.state.loaderData)
-    }
+        //After getting data make false isLoading
+        this.loadData(() =>
+            this.setState({ loaderData }),
+            loaderData.isLoading = false
+        )
+       
+        }
 
     componentDidMount() {
         this.init();
+        
     };
 
     loadData(callback) {
-        var link = 'http://localhost:51689/listing/listing/getSortedEmployerJobs';
+     
         var cookies = Cookies.get('talentAuthToken');
-       // your ajax call and other logic goes here
+        // your ajax call and other logic goes here
+
+        $.ajax({
+            url: 'https://madutalentservicestalent.azurewebsites.net/listing/listing/getSortedEmployerJobs',
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "GET",
+            contentType: "application/json",
+            data: {
+                activePage: this.state.activePage,
+                sortbyDate: this.state.sortBy.date,
+                showActive: this.state.filter.showActive,
+                showClosed: this.state.filter.showClosed,
+                showExpired: this.state.filter.showExpired,
+                showUnexpired: this.state.filter.showUnexpired
+
+            },
+            dataType: "json",
+            success: function (result) {
+
+                if (result.myJobs) {
+                    this.state.loadJobs = result.myJobs
+                        
+                }
+                console.log("result", this.state.loadJobs)
+                callback();
+            }.bind(this),
+
+            error: function (res) {
+                console.log("error");
+                callback();
+            }
+        })
+        
     }
 
     loadNewData(data) {
@@ -75,10 +112,100 @@ export default class ManageJob extends React.Component {
     }
 
     render() {
+        let loadJobs = this.state.loadJobs;
+        let jobCard = null;
+        if (loadJobs != "") {
+            jobCard = loadJobs.map((item, index) => (
+                <Card key={index}>
+
+                    <Card.Content>
+                        <Card.Header>{item.title}</Card.Header>
+                        <a className="ui black right ribbon label"><i className="user icon"></i></a>
+                        <Card.Meta>{item.location.city},{item.location.country}</Card.Meta>
+                        {/* <Card.Meta> Expirys On: {moment(item.expiryDate).format('LL')}</Card.Meta> */}
+                        <Card.Description>{item.summary}</Card.Description>
+
+                    </Card.Content>
+                    <Card.Content extra>
+                       
+                        <Button color="red" floated="left" size="mini">Expired</Button>
+                        <Button.Group floated="right" size="mini">
+                            <Button className="ui blue basic"><Icon name="ban" />Close</Button>
+                            <Button className="ui blue basic"><Icon name="edit" />Edit</Button>
+                            <Button className="ui blue basic"><Icon name="copy" />Copy</Button>
+                        </Button.Group>
+                       
+                    </Card.Content>
+                </Card>
+            )
+            )
+        }
+    
+
         return (
-            <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
-               <div className ="ui container">Your table goes here</div>
+            <BodyWrapper loaderData={this.state.loaderData} reload={this.init}>
+                <section className="page-body">
+                    <div className="ui container">
+                        <div className="ui container">
+                            <h1>List of Jobs</h1>
+                            {/* //Filter Active and Close Job */}
+                <i className="filter icon"></i> Filter: <b> Choose filter</b>
+
+                <div className="caret down icon ui simple dropdown item">
+                    <i className="caret down icon"></i>
+                    <div className="menu">
+                         <div className="item" >Active Jobs</div>
+                         <div className="item" >Closed Jobs</div>
+                    </div>
+                </div>
+{/* //Sort by oldest and newest Item */}
+                            
+                <i className="ui calendar left calendar icon"></i>
+             Sort By Jobs: <b>Newest </b>
+                <div className="caret down icon ui simple dropdown item">
+                      
+                    <i className="caret down icon"></i>
+                    <div className="menu">
+                      <div className="item" > Newest Jobs </div>
+                      <div className="item">  Oldest Job  </div>
+                    </div>
+                    <br/>
+                </div>
+
+  {/* //Card Showing here */}
+                       
+                            <div className="ui three cards" style={{margin:10}}>
+                                                       
+                                    {                                       
+                                        jobCard?jobCard:<span >No Jobs Found</span>
+                                        
+                                    }
+                              
+                            </div>
+                            {/*card sample*/}
+
+                         {/* Pagination Start */}
+                            
+
+                            <div align="center">
+                            <Pagination
+                                ellipsisItem={{ content: <Icon name='ellipsis horizontal' />, icon: true }}
+                                firstItem={{ content: <Icon name='angle double left' />, icon: true }}
+                                lastItem={{ content: <Icon name='angle double right' />, icon: true }}
+                                prevItem={{ content: <Icon name='angle left' />, icon: true }}
+                                nextItem={{ content: <Icon name='angle right' />, icon: true }}
+                                totalPages={this.state.totalPages}
+                                // activePage= {  jobCard?1:null}
+                                activePage= {  jobCard?this.state.activePage:null}
+                                                                 
+                            />
+                </div> 
+                            {/* End of Pagiantion */}
+                           
+                        </div>
+                    </div>
+                </section>
             </BodyWrapper>
-        )
+        );
     }
 }
